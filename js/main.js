d9196be8b65042a5073b6d8e76223a7c7c683054
Ana Rute Mendes <anarutecvm@gmail.com>
2016-07-12 14:12:59 -0300

    split js functions and hide dxr when null

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-function updateurl(input){
-
-  // let mxrulr = window.location;
-  //let mxr_url = "http://mxr.mozilla.org/comm-central/find?string=blah";
-  let mxr_url = input;
-
-  // The choice to repeat mozilla is to make a safer replacement, in case there is
-  // a mxr substring in any url that is not the MXR subdomain.
+function guess_dxr_url(mxr_url){
+  // The choice to repeat mozilla is to make a safer replacement, in case there
+  // is a mxr substring in any url that is not the MXR subdomain.
   let dxr_url = mxr_url.replace("mxr.mozilla", "dxr.mozilla");
 
-  // http://mxr.mozilla.org/comm-central/find?string=blah → https://dxr.mozilla.org/comm-central/search?q=file%3Ablah
+  // What we'll be searching for to replace
   var regexp = /search\?/;
   var regexp2 = /find\?/;
 
@@ -16,19 +11,22 @@ function updateurl(input){
     dxr_url = dxr_url.replace("string=", "q=");
   } else if (dxr_url.search(regexp2) != -1) {
     dxr_url = dxr_url.replace("find?string=", "search?q=file%3A");
-  }
+  };
 
-  document.getElementById("dxrurl").href = dxr_url;
-  document.getElementById("dxrurl").text = dxr_url;
-  // let urlresto = window.location.search.substring(0);
-  // console.log(urlresto);
-  //
-  // let urlquery = window.location.search.substring(1);
-  //
-  // const DXR_BASE_ULR = "https://dxr.mozilla.org/"
-  //
-  // console.log(DXR_BASE_ULR + urlquery);
-  console.log(mxr_url);
-  console.log(dxr_url);
+  return dxr_url;
+};
+
+function updateurl(input){
+
+  // When the project is finished we will use window.location as input
+  // let mxr_url = window.location;
+
+  var guess_url = guess_dxr_url(input);
+
+  document.getElementById("dxrurl_suggestion").style.display = "block";
+  
+  let dxr_url_el = document.getElementById("dxrurl");
+  dxr_url_el.href = guess_url;
+  dxr_url_el.text = guess_url;
 
 };
